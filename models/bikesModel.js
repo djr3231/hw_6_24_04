b8b043bdded580f1baf41e3d3aa4f6462ecf70de
Your name <you@example.com>
2023-05-02 16:40:23 +0300

    yrwh

--- models/bikesModel.js
+++ models/bikesModel.js
@@ -14,7 +14,7 @@ const bikeSchema = new mongoose.Schema({
     company:Joi.string().min(2).max(150).required(),
     model:Joi.string().min(2).max(150).required(),
     year: Joi.number().integer().min(1900).max(new Date().getFullYear()).required(),
-    price:Joi.number().min(1).max(999).required()
+    price:Joi.number().min(1).max(99999).required()
     })
     return joiSchema.validate(_reqBody);
   }
